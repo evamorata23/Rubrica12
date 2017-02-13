ed12b019b7d608a2918d6f8974a3455571deac8b
JoTerrance <gorka.zgz@gmail.com>
2017-02-13 18:56:04 +0100

    solved first error

--- src/main/java/rubrica12/webpages/HomePage.java
+++ src/main/java/rubrica12/webpages/HomePage.java
@@ -1,21 +1,9 @@
 package rubrica12.webpages;
 
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-import org.apache.wicket.markup.html.panel.FeedbackPanel;
-import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.spring.injection.annot.SpringBean;
-
-import de.agilecoders.wicket.extensions.markup.html.bootstrap.form.datetime.DatetimePicker;
-import rubrica12.model.Author;
-import rubrica12.repository.Repository;
 
 public class HomePage extends WebPage {
-	@SpringBean
-	Repository repo;
 	
 
 	public HomePage() {
